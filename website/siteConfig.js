9f447f8e71d4a81ca098bb85986043ed240034f1
Pranav Shukla <pranav.shukla@valensdatalabs.com>
2020-04-07 17:15:57 +0530

    updated to JA repository

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Guzzle', // Title for your website.
   tagline: 'Guzzle Documentation',
-  url: 'https://pranavshukla-org.github.io', // Your website URL
-  baseUrl: '/guzzle-documentation/', // Base URL for your project */
+  url: 'https://justanalytics.github.io', // Your website URL
+  baseUrl: '/guzzle-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'guzzle-documentation',
-  organizationName: 'pranavshukla-org',
+  projectName: 'guzzle-docs',
+  organizationName: 'justanalytics',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
